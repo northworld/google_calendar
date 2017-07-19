76e3b9b56f9bcf429c1927269025afc69abde863
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-07-19 16:28:07 +0900

    Remove unused errors
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- lib/google/errors.rb
+++ lib/google/errors.rb
@@ -2,7 +2,4 @@ module Google
   class HTTPRequestFailed < StandardError; end
   class HTTPAuthorizationFailed < StandardError; end
   class HTTPNotFound < StandardError; end
-  class HTTPTooManyRedirections < StandardError; end
-  class InvalidCalendar < StandardError; end
-  class CalenarIDMissing < StandardError; end
 end
