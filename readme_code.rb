95a40789d560aed32d66e52d1bd1daf8848e404c
Justin Hirsch <justin@northworld.com>
2022-09-28 09:14:55 -0700

    Update README and readme_code to provide an example setup which does not use the depricated 'oob' OAuth method

--- readme_code.rb
+++ readme_code.rb
@@ -12,7 +12,7 @@ require 'google_calendar'
 cal = Google::Calendar.new(:client_id     => YOUR_CLIENT_ID,
                            :client_secret => YOUR_SECRET,
                            :calendar      => YOUR_CALENDAR_ID,
-                           :redirect_url  => "urn:ietf:wg:oauth:2.0:oob" # this is what Google uses for 'applications'
+                           :redirect_url  => YOUR_REDIRECT_URL # this is what Google uses for 'applications'
                            )
 
 puts "Do you already have a refresh token? (y/n)"
